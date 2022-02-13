f75140942e35c18c19d5549e0828f1f4113d6f81
Brian Sunbury <bsunbury29@gmail.com>
2022-02-13 09:59:48 -0700

    custom Authenticator flow

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { isMobile } from "react-device-detect";
 import Amplify from "aws-amplify";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import awsExports from "./src/aws-exports";
 import {
   NavDrawer,
@@ -25,7 +25,22 @@ const wrapPageElement = ({ element }) => {
 };
 
 const wrapRootElement = ({ element }) => (
-  <Authenticator.Provider>{element}</Authenticator.Provider>
+  <Authenticator.Provider>
+    <Authenticated>{element}</Authenticated>
+  </Authenticator.Provider>
 );
+
+const Authenticated = ({ children }) => {
+  const { route } = useAuthenticator(context => [context.route]);
+
+  /*
+    1. there's a problem with amplify's Router, but we
+      can fix it by doing it ourselves.
+    2. We don't want to show the Authenticator if the user is not logged in initially (
+      so they can use they app before) logging in, 
+      but we want to render if quickly if they are authenticated so we get the user info
+  */
+  return ["idle", "setup"].includes(route) ? <Authenticator /> : children;
+};
 
 export { wrapPageElement, wrapRootElement };
