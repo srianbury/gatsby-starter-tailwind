1656c8314df606447b78e1466c8e0c503929d468
Brian Sunbury <bsunbury29@gmail.com>
2022-02-16 22:10:07 -0700

    custom mobile login

--- src/components/header/LoginOrUser.js
+++ src/components/header/LoginOrUser.js
@@ -2,7 +2,10 @@ import * as React from "react";
 import { useContext } from "react";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { Button } from "@mui/material";
+import { isMobile } from "react-device-detect";
 import { LoginModalContext } from "../LoginModal";
+import { navigate } from "gatsby-link";
+import { NavDrawerContext } from "../navDrawer";
 
 const LoginOrUser = () => {
   const { route } = useAuthenticator(context => [context.route]);
@@ -27,9 +30,19 @@ HeaderLogoutButton.propTypes = {};
 
 const OpenLoginModalButton = () => {
   const { openModal } = useContext(LoginModalContext);
+  const { closeDrawer } = useContext(NavDrawerContext);
+
+  function goToLogInPage() {
+    navigate("/login");
+    closeDrawer();
+  }
+
+  function handleClick() {
+    isMobile ? goToLogInPage() : openModal();
+  }
 
   return (
-    <Button onClick={openModal} className="dark:border-white dark:text-white">
+    <Button onClick={handleClick} className="dark:border-white dark:text-white">
       <span className="font-bold">Log In</span>
     </Button>
   );
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,28 @@
 import * as React from "react";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { useEffect } from "react";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
+import { navigate } from "gatsby";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import "@aws-amplify/ui-react/styles.css";
 
-const Login = () => (
-  <Layout>
-    <Seo title="About" />
-    <Authenticator />
-  </Layout>
-);
+const Login = () => {
+  const { route } = useAuthenticator(context => [context.route]);
+
+  // close the modal after the user logs in
+  useEffect(() => {
+    if (route === "authenticated") {
+      navigate("/");
+    }
+  }, [route]);
+
+  return (
+    <Layout>
+      <Seo title="Login" />
+      <Authenticator />
+    </Layout>
+  );
+};
 
 export default Login;
