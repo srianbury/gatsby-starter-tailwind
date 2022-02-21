97bb0af1ba564d1b8a03e22d76fe57f4a25a9dd4
Brian Sunbury <bsunbury29@gmail.com>
2022-02-21 00:50:26 -0700

    read error message

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,45 +15,61 @@ const IndexPage = () => {
   );
 };
 
+const INIT_POST_STATE = {
+  data: null,
+  error: null,
+};
 const ListPosts = () => {
   const { user } = useAuthenticator(context => [context.user]);
-  const [posts, setPosts] = useState(null);
+  const [posts, setPosts] = useState(INIT_POST_STATE);
 
   useEffect(() => {
     async function read() {
       try {
+        setPosts(INIT_POST_STATE);
         const result = await API.graphql({
           query: listPosts,
           variables: {},
           authMode: user ? "AMAZON_COGNITO_USER_POOLS" : "AWS_IAM",
         });
+        setPosts(cur => ({
+          ...cur,
+          data: result.data.listPosts.items,
+        }));
         console.log({ result });
-        setPosts(result.data.listPosts.items);
       } catch (e) {
-        setPosts([]);
+        setPosts(cur => ({
+          ...cur,
+          error: "An unexpecte error occurred",
+        }));
         console.log("listPosts", { e });
       }
     }
     read();
   }, []);
 
-  if (posts === null) {
+  if (posts.data === null) {
     return <div>Loading...</div>;
   }
 
-  if (posts.length === 0) {
+  if (posts.error !== null) {
+    return <div>{posts.error}</div>;
+  }
+
+  if (posts.data.length === 0) {
     return <div>No posts.</div>;
   }
 
   return (
     <div>
-      {posts.map(post => (
+      {posts.data.map(post => (
         <div key={post.id} className="border-rounded mb-2 border p-2">
           <h2>{post.title}</h2>
           <div>{post.body}</div>
-          <div>{`Source ${post.source}`}</div>
+          <div>{`Source: ${post.source}`}</div>
           <div>{`Created at: ${post.createdAt}`}</div>
           <div>{post.owner}</div>
+          <div>{post.id}</div>
         </div>
       ))}
     </div>
