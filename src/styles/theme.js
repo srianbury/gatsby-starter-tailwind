981574fe651bef50f180f98294d9b65f01296b96
Brian Sunbury <bsunbury29@gmail.com>
2022-02-20 18:10:05 -0700

    use default styles

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,18 +3,12 @@ import { createTheme } from "@mui/material/styles";
 const lightTheme = createTheme({
   palette: {
     mode: "light",
-    primary: {
-      main: "#ff0000",
-    },
   },
 });
 
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
-    primary: {
-      main: "#0000ff",
-    },
   },
 });
 
